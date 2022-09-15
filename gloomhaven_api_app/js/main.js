850e208e9ef29bcc0f5b4502c6426a092c639227
Desiree P <dpricelcsw@gmail.com>
2022-09-15 15:43:24 -0500

    css, html, choices

--- gloomhaven_api_app/js/main.js
+++ gloomhaven_api_app/js/main.js
@@ -1,21 +1,33 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('#cardButton').addEventListener('click', getFetch)
+document.querySelector('#optionAButton').addEventListener('click', optionA)
+document.querySelector('#optionBButton').addEventListener('click', optionB)
+
+let optionAText = ''
+let optionBText = ''
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  console.log(choice)
-  const url = `http://gloomhavendb.com/api/events/road/${Math.floor(Math.random()*69)}`
+  let cardNumber = Math.floor(Math.random()*69)
+  //const choice = document.querySelector('input').value
+  //console.log(choice)
+  const url = `http://gloomhavendb.com/api/events/road/${cardNumber}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        console.log(data.id)
-        //let imgUrl = 'http://gloomhavendb.com' + data.imageUrl
+        optionAText = data.optionA.outcome
+        optionBText = data.optionB.outcome
         document.querySelector('img').src = 'http://gloomhavendb.com' + data.imageUrl
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+    }
+
+function optionA () {
+  document.querySelector('h3').innerText = optionAText
 }
 
+function optionB() {
+  document.querySelector('h3').innerText = optionBText
+}
